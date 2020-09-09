d1ec819d5ccfe69b841eae04fdd30861de96e98a
davide-amabile <davideamabile81@gmail.com>
2020-09-09 08:47:33 +0200

    iserimento messaggi senza grafica con click e keyup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function (){
-  
+  // creare evento click
+  $("#sender").click(
+    function(){
+      var input = $("#text_send").val();
+      console.log(input);
+      var a = $(".template li ").clone();
+      a.prepend(input+" ");
+      $(".lista_messaggi").append(a);
+      $("#text_send").val("");
+    });
+
+
+    // creare evento keyup dentro l'input
+    $("#text_send").keyup(
+      function(evento){
+        if(evento.which == 13) {
+          var input = $("#text_send").val();
+          var a = $(".template li").clone();
+          a.prepend(input+" ");
+          $(".lista_messaggi").append(a);
+          $("#text_send").val("");
+        }
+      });
 });
