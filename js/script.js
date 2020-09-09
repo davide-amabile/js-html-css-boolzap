8c10d1430d84c8371fdc5e52b803d51d4fb98f97
davide-amabile <davideamabile81@gmail.com>
2020-09-09 16:35:47 +0200

    classe green

--- js/script.js
+++ js/script.js
@@ -7,7 +7,11 @@ $(document).ready(function (){
       var a = $(".template p").clone();
       console.log(a);
       a.prepend(input);
-      $("#testo").append(a);
+      $("#testo_inv").append(a);
+      // aggiungo l classe per il background verde
+      $(".message_row .messaggio").addClass("green");
+      // aggiungo la classe end per farlo apparire a destra
+      $(".second_box_destra").children(".message_row ").addClass("end");
       $("#text_send").val("");
      })
     });
