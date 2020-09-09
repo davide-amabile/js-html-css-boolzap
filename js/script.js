cacb2356357f627311583fd59c032f45ec6338b2
davide-amabile <davideamabile81@gmail.com>
2020-09-09 22:58:49 +0200

    risposta

--- js/script.js
+++ js/script.js
@@ -2,16 +2,33 @@ $(document).ready(function (){
   // creare evento click
   $("#sender").click(
     function(){
+      // prendere ilvalore
       var input = $("#text_send").val();
-      console.log(input);
-      var a = $(".template p").clone();
-      console.log(a);
-      a.prepend(input);
-      $("#testo_inv").append(a);
-      // aggiungo l classe per il background verde
-      $(".message_row .messaggio").addClass("green");
-      // aggiungo la classe end per farlo apparire a destra
-      $(".second_box_destra").children(".message_row ").addClass("end");
+      if ( input != "") {
+
+      // clonare il template
+      var templateClonato = $(".template .message_row").clone();
+      // inserire il valore nel template
+      templateClonato.find(".testo_inv1").text(input);
+      templateClonato.find(".orologio1").text("21.00");
+
+      // aggiungere la classe right per stamparlo a destra e con il background verde
+      $(".message_row").addClass("right");
+      $(".second_row").removeClass("right");
+      $(".second_row").hide();
+
+      // aggiungere il template nel box_destra_up
+      $(".second_box_destra").append(templateClonato);
+      // svuotare il value uma volta cliccato
       $("#text_send").val("");
+
+      // creare funzione asincrona per far apparire la risposta
+      setTimeout(function(){
+        $(".second_row").show();
+      },1000);
+
+      }
      })
+     // crea evento ricerca per lettere
+
     });
