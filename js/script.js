c0195a99e8fa8d228debb9e580bbc5b9154d653f
davide-amabile <davideamabile81@gmail.com>
2020-09-10 12:14:06 +0200

    risposta

--- js/script.js
+++ js/script.js
@@ -13,9 +13,9 @@ $(document).ready(function (){
       templateClonato.find(".orologio1").text("21.00");
 
       // aggiungere la classe right per stamparlo a destra e con il background verde
-      $(".message_row").addClass("right");
-      $(".second_row").removeClass("right");
-      $(".second_row").hide();
+      templateClonato.addClass("right");
+      // $(".second_row").removeClass("right");
+      // $(".second_row").hide();
 
       // aggiungere il template nel box_destra_up
       $(".second_box_destra").append(templateClonato);
@@ -24,7 +24,13 @@ $(document).ready(function (){
 
       // creare funzione asincrona per far apparire la risposta
       setTimeout(function(){
-        $(".second_row").show();
+        // riclonare il template come risposta
+        var templateRisp = $(".template .message_row").clone();
+        // aggiungere il testo ok
+        templateRisp.find(".testo_inv1").text("ok");
+        templateRisp.find(".orologio1").text("21.00");
+        // appedo sempre al box chat
+        $(".second_box_destra").append(templateRisp);
       },1000);
 
       }
