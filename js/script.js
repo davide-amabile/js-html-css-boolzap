7465c5ccd9d5287a06e271dd3c1046849d7d13c0
davide-amabile <davideamabile81@gmail.com>
2020-09-15 09:01:07 +0200

    html

--- js/script.js
+++ js/script.js
@@ -36,6 +36,44 @@ $(document).ready(function (){
       // fine evento
      })
     // creare evento keyup su input e far stampare il messaggio
+    $("#sender").keypress(
+      function(evento){
+        if (evento.which == 13){
+
+          var input = $("#text_send").val();
+          if ( input != "") {
+
+          // clonare il template
+          var templateClonato = $(".template .message_row").clone();
+          // inserire il valore nel template
+          templateClonato.find(".testo_inv1").text(input);
+          templateClonato.find(".orologio1").text("21.00");
+
+          // aggiungere la classe right per stamparlo a destra e con il background verde
+          templateClonato.addClass("right");
+          // $(".second_row").removeClass("right");
+          // $(".second_row").hide();
+
+          // aggiungere il template nel box_destra_up
+          $(".second_box_destra.active").append(templateClonato);
+          // svuotare il value uma volta cliccato
+          $("#text_send").val("");
+
+          // creare funzione asincrona per far apparire la risposta
+          setTimeout(function(){
+            // riclonare il template come risposta
+            var templateRisp = $(".template .message_row").clone();
+            // aggiungere il testo ok
+            templateRisp.find(".testo_inv1").text("ok");
+            templateRisp.find(".orologio1").text("21.00");
+            // appedo sempre al box chat
+            $(".second_box_destra.active").append(templateRisp);
+          },1000);
+          }
+        }
+      }
+    );
+    // fine evento
 
 
      // crea evento ricerca per lettere
